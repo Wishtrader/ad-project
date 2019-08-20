1d971cdcfd7ed98ae610bd717eb5f468fa509f1c
Wishtrader <mr.akomarov@gmail.com>
2019-08-20 23:23:47 +0300

    Added navbar and navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
+    <v-navigation-drawer temporary app v-model="drawer">
+      <v-card flat class="mx-auto" max-width="300" tile>
+        <v-list>
+          <v-subheader>MENU</v-subheader>
+          <v-list-item-group color="primary" v-for="link in links" :key="link.id" :to="link.url">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>{{ link.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>{{ link.title }}
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
     </v-navigation-drawer>
 
-    <v-app-bar app>
-      <!-- -->
-    </v-app-bar>
+    <!-- TOOLBAR START -->
+    <div>
+      <v-toolbar app dark color="primary">
+        <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>AD APPLICATION</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down" v-for="link in links" :key="link.id" :to="link.url">
+          <v-btn text>
+            <v-icon left>{{ link.icon }}</v-icon>
+            {{ link.title }}
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </div>
+    <!-- TOOLBAR END -->
 
     <!-- Sizes your content based upon application components -->
     <v-content>
@@ -27,6 +52,17 @@
 
 <script>
   export default {
-  
+    data() {
+      return {
+        drawer: false,
+        links: [
+          {title: 'Login', icon: 'mdi-lock', url: '/login'},
+          {title: 'Register', icon: 'mdi-face', url: '/register'},
+          {title: 'Orders', icon: 'mdi-bookmark-multiple', url: '/orders'},
+          {title: 'New Ad', icon: 'mdi-file-plus', url: '/new'},
+          {title: 'My Ads', icon: 'mdi-wunderlist', url: '/list'},
+        ]
+      }
+    }
   };
 </script>
