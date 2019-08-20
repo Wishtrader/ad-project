d9b545cfde83737eef717e9456ad1bf89114f14d
Wishtrader <mr.akomarov@gmail.com>
2019-08-21 00:58:11 +0300

    Added routes

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
       <v-card flat class="mx-auto" max-width="300" tile>
         <v-list>
           <v-subheader>MENU</v-subheader>
-          <v-list-item-group color="primary" v-for="link in links" :key="link.id" :to="link.url">
-            <v-list-item>
+          <v-list-item-group color="primary">
+            <v-list-item v-for="link in links" :key="link.title" :to="link.url">
               <v-list-item-icon>
                 <v-icon>{{ link.icon }}</v-icon>
               </v-list-item-icon>
@@ -19,12 +19,12 @@
 
     <!-- TOOLBAR START -->
     <div>
-      <v-toolbar app dark color="primary">
+      <v-toolbar dark color="primary">
         <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
         <v-toolbar-title>AD APPLICATION</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-sm-and-down" v-for="link in links" :key="link.id" :to="link.url">
-          <v-btn text>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn v-for="link in links" :key="link.title" :to="link.url" text>
             <v-icon left>{{ link.icon }}</v-icon>
             {{ link.title }}
           </v-btn>
@@ -37,11 +37,11 @@
     <v-content>
 
       <!-- Provides the application the proper gutter -->
-      <v-container fluid>
+      
 
         <!-- If using vue-router -->
-        <router-view></router-view>
-      </v-container>
+        <router-view />
+      
     </v-content>
 
     <v-footer app>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Auth/Login'
+import Register from '@/components/Auth/Register'
+import Ad from '@/components/Ads/Ad'
+import AdList from '@/components/Ads/AdList'
+import NewAd from '@/components/Ads/NewAd'
+import Orders from '@/components/User/Orders'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/ad/:id',
+      name: 'ad',
+      component: Ad
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: AdList
+    },
+    {
+      path: '/new',
+      name: 'newad',
+      component: NewAd
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: Orders
+    }
+  ]
 })
